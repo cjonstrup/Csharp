f7a8cbd2eae7d396b88190232524d741e9b0e94d
Carsten Jonstrup <carsten@lenius.dk>
2015-10-12 23:57:39 +0200

    Init - Admin Application

---
+++ Examples/AdminApplication/App.xaml.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Security.Principal;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace AdminApplication
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+        protected override void OnStartup(StartupEventArgs e)
+        {
+            base.OnStartup(e);
+
+            if (!IsAdministrator())
+            {
+                MessageBox.Show("Need Administrator rights");
+                this.Shutdown(0);
+            }
+        }
+
+        public bool IsAdministrator()
+        {
+            bool isAdmin;
+            try
+            {
+                var user = WindowsIdentity.GetCurrent();
+                var principal = new WindowsPrincipal(user);
+                isAdmin = principal.IsInRole(WindowsBuiltInRole.Administrator);
+            }
+            catch (Exception ex)
+            {
+                isAdmin = false;
+            }
+            return isAdmin;
+        }
+    }
+}
---
+++ Examples/AdminApplication/README.md
@@ -0,0 +1,18 @@
+﻿#Admin Application
+
+## Description
+
+Howto prevent your application from not running in administrator mode.
+
+```csharp
+protected override void OnStartup(StartupEventArgs e)
+{
+    base.OnStartup(e);
+
+    if (!IsAdministrator())
+    {
+        MessageBox.Show("Need Administrator rights");
+        Shutdown(0);
+    }
+}
+```
