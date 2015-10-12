f2cbdf49211b3e399ae82c77e9765e75e289fca8
Carsten Jonstrup <carsten@lenius.dk>
2015-10-12 23:03:58 +0200

    Cleanup

--- Examples/PropertyGrid/PropertyGrid/MainWindow.xaml.cs
+++ Examples/PropertyGrid/PropertyGrid/MainWindow.xaml.cs
@@ -1,23 +1,10 @@
 ﻿using System;
-using System.CodeDom;
-using System.Collections.Generic;
 using System.ComponentModel;
 using System.Globalization;
-using System.Linq;
 using System.Runtime.CompilerServices;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Forms;
-using System.Windows.Input;
 using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
-using ActiproSoftware.Windows.Media;
 using PropertyGrid.Annotations;
 using MouseEventArgs = System.Windows.Input.MouseEventArgs;
 
