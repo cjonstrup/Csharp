11364bd50b566086916d95c987b4d3213d429cef
Carsten Jonstrup <carsten@lenius.dk>
2015-10-12 22:17:08 +0200

    Init

---
+++ PropertyGrid/PropertyGrid/MainWindow.xaml.cs
@@ -0,0 +1,132 @@
+﻿using System;
+using System.CodeDom;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Globalization;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Forms;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using ActiproSoftware.Windows.Media;
+using PropertyGrid.Annotations;
+using MouseEventArgs = System.Windows.Input.MouseEventArgs;
+
+namespace PropertyGrid
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+
+        private Test ViewModel;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            Loaded += MainWindow_Loaded;
+
+            DataContext = ViewModel = new Test();
+
+        }
+
+        private void MainWindow_Loaded(object sender, RoutedEventArgs e)
+        {
+            propertyGrid.SelectedObject = new Test();
+            propertyGrid.PropertyValueChanged += PropertyGrid_PropertyValueChanged;
+        }
+
+        private void PropertyGrid_PropertyValueChanged(object s, System.Windows.Forms.PropertyValueChangedEventArgs e)
+        {
+            var grid = s as System.Windows.Forms.PropertyGrid;
+
+          
+               // ViewModel.GetType().GetProperty(e.ChangedItem.ToString()).SetValue(ViewModel, e.ChangedItem.Value, null);
+
+        }
+
+        private void MainWindow_OnMouseMove(object sender, MouseEventArgs e)
+        {
+            // Retrieve the coordinate of the mouse position.
+            Point pt = e.GetPosition((UIElement)sender);
+
+            // Perform the hit test against a given portion of the visual object tree.
+            HitTestResult result = VisualTreeHelper.HitTest(this, pt);
+
+            if (result != null)
+            {
+                if (result.VisualHit.GetType() == typeof (TextBlock))
+                {
+                    propertyGrid.SelectedObject = result.VisualHit;
+                } 
+                
+                // Perform action on hit visual object.
+                
+            }
+        }
+    }
+
+
+
+
+    public class Test : INotifyPropertyChanged
+    {
+        private string _tagName;
+
+        public string TagName
+        {
+            get { return _tagName; }
+            set
+            {
+                if (value == _tagName) return;
+                _tagName = value;
+                OnPropertyChanged();
+            }
+        }
+
+        [TypeConverter(typeof(DrinkerClassConverter))]
+        public bool Enabled { get; set; }
+        public double Value { get; set; }
+
+        public Color Background { get; set; }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        [NotifyPropertyChangedInvocator]
+        protected virtual void OnPropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+
+    class DrinkerClassConverter : BooleanConverter
+    {
+        public override object ConvertTo(ITypeDescriptorContext context,
+        CultureInfo culture,
+        object value,
+        Type destType)
+        {
+            return (bool)value ?
+            "Yes" : "Yes, of course";
+        }
+
+        public override object ConvertFrom(ITypeDescriptorContext context,
+        CultureInfo culture,
+        object value)
+        {
+            return (string)value == "Yes";
+        }
+    }
+
+  
+}
